b763052623ec3c143d32c54183d071187956d90e
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-02-21 13:03:41 +0300

    final

---
+++ Callback functions/Checker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callback_functions
+{
+    class Checker
+    {
+        public bool Any(int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                if (num > 3)
+                {
+                    return true;
+                }  
+            }
+            return false;
+        }
+
+        public bool All(int[] nums)
+        {
+            int count = 0;
+            foreach (int num in nums)
+            {
+                if (num > 3)
+                {
+                    count += 1;
+                }
+                if (count == nums.Length)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Callback functions/Program.cs
+++ Callback functions/Program.cs
@@ -8,8 +8,42 @@ namespace Callback_functions
 {
     class Program
     {
+        delegate bool Condition(int[] massive);
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter size of massive:");
+            int i, size = Convert.ToInt32(Console.ReadLine());
+            int[] massive = new int[size];
+            Console.WriteLine("Enter elements through space::");
+            string[] num = Console.ReadLine().Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+            for (i = 0; i < massive.Length; ++i)
+            {
+                massive[i] = Convert.ToInt32(num[i]);
+            }
+
+            Checker checker = new Checker();
+
+            Condition condition = checker.Any;
+            bool checkerCondition = condition(massive);
+            if (checkerCondition == true)
+            {
+                Console.WriteLine("Any true");
+            }
+            else
+            {
+                Console.WriteLine("Any false");
+            }
+            condition = checker.All;
+            if (checkerCondition == true)
+            {
+                Console.WriteLine("All true");
+            }
+            else
+            {
+                Console.WriteLine("Not all true");
+            }
+            Console.ReadKey();
         }
     }
 }
